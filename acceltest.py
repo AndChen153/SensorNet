eb9490ded8f94cdc65e3336fc43c8537f7470e23
andrew <achen1192@gmail.com>
2021-02-05 17:56:30 -0600

    2nd

--- acceltest.py
+++ acceltest.py
@@ -53,7 +53,7 @@ def get_x_rotation(x,y,z):
 bus = smbus.SMBus(1) # bus = smbus.SMBus(0)
 address = 0x68       # via i2cdetect
 bus2 = smbus.SMBus(1) # bus = smbus.SMBus(0)
-address2 = 0x68       # via i2cdetect
+address2 = 0x69       # via i2cdetect
 
 
 while True:
