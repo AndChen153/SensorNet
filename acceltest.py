762f9020ca18b45c94e5f14b12c1036d6cde0bb1
andrew <achen1192@gmail.com>
2021-02-05 17:50:31 -0600

    2nd

--- acceltest.py
+++ acceltest.py
@@ -22,6 +22,22 @@ def read_word_2c(reg):
         return -((65535 - val) + 1)
     else:
         return val
+
+def read_byte2(reg):
+    return bus2.read_byte_data(address, reg)
+ 
+def read_word2(reg):
+    h = bus2.read_byte_data(address, reg)
+    l = bus2.read_byte_data(address, reg+1)
+    value = (h << 8) + l
+    return value
+ 
+def read_word_2c2(reg):
+    val = read_word2(reg)
+    if (val >= 0x8000):
+        return -((65535 - val) + 1)
+    else:
+        return val
  
 def dist(a,b):
     return math.sqrt((a*a)+(b*b))
@@ -35,7 +51,10 @@ def get_x_rotation(x,y,z):
     return math.degrees(radians)
  
 bus = smbus.SMBus(1) # bus = smbus.SMBus(0)
-address = 0x69       # via i2cdetect
+address = 0x68       # via i2cdetect
+bus2 = smbus.SMBus(2) # bus = smbus.SMBus(0)
+address2 = 0x68       # via i2cdetect
+
 
 while True:
     # Activate to be able to address the module
@@ -71,4 +90,38 @@ while True:
     print "X Rotation: " , get_x_rotation(acceleration_xout_scaled, acceleration_yout_scaled, acceleration_zout_scaled)
     print "Y Rotation: " , get_y_rotation(acceleration_xout_scaled, acceleration_yout_scaled, acceleration_zout_scaled)
 
-    time.sleep(0.5)
+
+
+    bus2.write_byte_data(address2, power_mgmt_1, 0)
+
+    print "Gyro"
+    print "--------"
+    
+    gryo_xout = read_word_2c2(0x43)
+    gryo_yout = read_word_2c2(0x45)
+    gryo_zout = read_word_2c2(0x47)
+    
+    print "gryo_xout2: ", ("%5d" % gryo_xout), " scaled: ", (gryo_xout / 131)
+    print "gryo_yout2: ", ("%5d" % gryo_yout), " scaled: ", (gryo_yout / 131)
+    print "gryo_zout2: ", ("%5d" % gryo_zout), " scaled: ", (gryo_zout / 131)
+    
+    print
+    print "Accelerometer"
+    print "---------------------"
+    
+    acceleration_xout = read_word_2c2(0x3b)
+    acceleration_yout = read_word_2c2(0x3d)
+    acceleration_zout = read_word_2c2(0x3f)
+    
+    acceleration_xout_scaled = acceleration_xout / 16384.0
+    acceleration_yout_scaled = acceleration_yout / 16384.0
+    acceleration_zout_scaled = acceleration_zout / 16384.0
+    
+    print "acceleration_xout2: ", ("%6d" % acceleration_xout), " Scaled: ", acceleration_xout_scaled
+    print "acceleration_yout2: ", ("%6d" % acceleration_yout), " Scaled: ", acceleration_yout_scaled
+    print "acceleration_zout2: ", ("%6d" % acceleration_zout), " Scaled: ", acceleration_zout_scaled
+    
+    print "X Rotation2: " , get_x_rotation(acceleration_xout_scaled, acceleration_yout_scaled, acceleration_zout_scaled)
+    print "Y Rotation2: " , get_y_rotation(acceleration_xout_scaled, acceleration_yout_scaled, acceleration_zout_scaled)
+
+    time.sleep(5)
