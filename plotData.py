3702d8f4f4a9e3d1b261e4ac4a59c1382a7f41eb
andrew <achen1192@gmail.com>
2021-02-26 15:27:10 -0600

    test

--- plotData.py
+++ plotData.py
@@ -36,7 +36,36 @@ def plot_subject(subject):
     plt.show()
 
 subjects = load_dataset()
-#print(subjects[0])
-plot_subject(subjects[0])
-plot_subject(subjects[1])
-plot_subject(subjects[2])
+
+def plot_activity(subject):
+    time = []
+    x = []
+    y = []
+    z = []
+    for row in subject:
+        time.append(float(row[0]))
+        x.append(float(row[1]))
+        y.append(float(row[2]))
+        z.append(float(row[3]))
+        activity = row[4]
+
+    fig, (ax0, ax1, ax2) = plt.subplots(nrows=3, figsize=(15, 7), sharex=True)
+    plot_axis(ax0, time, x, 'X-Axis')
+    plot_axis(ax1, time, y, 'Y-Axis')
+    plot_axis(ax2, time, z, 'Z-Axis')
+    plt.subplots_adjust(hspace=0.2)
+    fig.suptitle(activity)
+    plt.subplots_adjust(top=0.90)
+    plt.show()
+
+def plot_axis(ax, x, y, title):
+    ax.plot(x, y, 'g')
+    ax.set_title(title)
+    ax.xaxis.set_visible(False)
+    ax.set_ylim([min(y) - np.std(y), max(y) + np.std(y)])
+    ax.set_xlim([min(x), max(x)])
+    ax.grid(True)
+
+
+plot_activity(subjects[0])
+plot_activity(subjects[5])
